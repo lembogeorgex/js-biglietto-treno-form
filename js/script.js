667d23891300126fc1843c8136cf28b1a7785263
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-29 17:18:52 +0200

    first input event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log ("JS OK")
+// Console check
+console.log("JS OK");
+
+
+// Mod targeting element
+document.getElementById("userName");
+document.getElementById("kmts");
+document.getElementById("age");
+
+
+
+// Name check
+const targetName = userName.value
+
+// Kilometers check
+const userKM = kmts.value
+
+
+// Age Check
+const userAge = age.value
+
+
+
+// Event Function
+
+const button =  document.getElementById("button");
+
+button.addEventListener("click", function() {
+    console.log(userName.value)
+})

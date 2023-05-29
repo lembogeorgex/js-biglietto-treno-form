620cf1d060a7afd4121774bac444c46ff25c36d2
lembogeorgex <lembogiorgio2@gmail.com>
2023-05-29 17:50:08 +0200

    print on page

--- js/script.js
+++ js/script.js
@@ -2,30 +2,28 @@
 console.log("JS OK");
 
 
-// Mod targeting element
-document.getElementById("naming");
-document.getElementById("kmts");
-document.getElementById("age");
 
 
 
-// Name check
+// Name input
 const targetName =  document.getElementById("naming");
 
-// Kilometers check
+// Kilometers input
 const userKM = document.getElementById("kmts");
 
-// Age Check
+// Age input
 const userAge =document.getElementById("age");
 
 
+// MOD Result
 // Event Function
 
 const button =  document.getElementById("button");
 
 button.addEventListener("click", function() {
-    console.log(targetName.value)
-    console.log(userKM.value)
-    console.log(userAge.value)
+    document.getElementById("nameResult").innerText = (targetName.value)
+    document.getElementById("kmtsResult").innerText = (userKM.value)
+    document.getElementById("ageResult").innerText = (userAge.value)
 
 })
+
